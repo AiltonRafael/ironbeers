ff39d8f07fa3a3fbbcf5ed3f8d108c8fb1b1d0c5
AiltonRafael <ailtonrafaeldiasjr@gmail.com>
2021-11-17 19:50:56 -0300

    updated

--- src/components/RandomBeerComponent/index.tsx
+++ src/components/RandomBeerComponent/index.tsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { CardBeerDetail } from '../BeerDetailComponent/CardBeerDetail';
 import { NavbarComponent } from '../Navbar/NavbarComponent';
+import axios from 'axios';
+import { Spinner } from '@chakra-ui/spinner';
 
 export const RandomComponent = () => {
+    const [dataInfo, setDataInfo] = useState<any>({});
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        axios.get('https://ih-beers-api2.herokuapp.com/beers/random')
+            .then((response) => setDataInfo(response.data))
+            .catch((err) => err)
+            setLoading(true)
+    }, [])
+
     return (
         <div>
             <NavbarComponent />
-            <CardBeerDetail 
-             image='oi'
-             name='oi'
-             tagline='oi'
-             first_brewed='oi'
-             description='oi'
-             attenuation_level='oi'
-             contributed_by='oi'
-            />
-          
+            { loading ? 
+               <CardBeerDetail 
+               image={dataInfo.image_url}
+               name={dataInfo.name}
+               tagline={dataInfo.tagline}
+               first_brewed={dataInfo.first_brewed}
+               description={dataInfo.description}
+               attenuation_level={dataInfo.attenuation_level}
+               contributed_by={dataInfo.contributed_by}
+              />
+            : <Spinner size='xl'/>}          
         </div>
     )
 }
